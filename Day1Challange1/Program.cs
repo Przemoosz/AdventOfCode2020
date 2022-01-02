b39ecad0d3b4808a947fc44465b0595b53547a98
Przemysław Szewczak <szewczakprzemyslaw1@gmail.com>
2022-01-02 10:25:16 +0100

    Finished Challange 1 and 2 for Day 1

---
+++ Day1Challange1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal class SolutionDay1
+    {
+        public static void Main(string[] args)
+        {
+            // Utils.FileRead();
+            string[] data = Utils.FileRead();
+            List <int> convertedData = IntListCreate(data);
+            // foreach(int i in convertedData)
+            //     Console.WriteLine(i+20);
+            foreach (var value in convertedData)
+            {
+                if (convertedData.Contains(2020 - value))
+                {
+                    int secondElement = 2020 - value;
+                    Console.WriteLine($"First Element {value}");
+                    Console.WriteLine($"Second Element {secondElement}");
+                    Console.WriteLine($"Answer: {value*secondElement}");
+                    break;
+                }
+
+            }
+           //
+        }
+
+        public static List<int> IntListCreate(string[] loadedData)
+        {
+            List<int> toReturn = new List<int>(loadedData.Length);
+            foreach (string number in loadedData)
+            {
+                toReturn.Add(Convert.ToInt32(number));
+            }
+            //foreach(int a in toReturn) Console.WriteLine(a.GetType);
+            return toReturn;
+        }
+    }
+
+    static class Utils
+    {
+        public static string[] FileRead()
+        {
+            int rows = File.ReadLines(@"C:\Przemek\Net and C#\AdventOfCode\Day1Challange1\input.txt").Count();
+            string[] lines = new string[rows];
+            int index = 0;
+            //Console.WriteLine(rows);
+            foreach (string row in File.ReadLines(@"C:\Przemek\Net and C#\AdventOfCode\Day1Challange1\input.txt"))
+            {
+                lines[index] = row.Trim();
+                index++;
+            }
+            // foreach (string row in lines) Console.WriteLine(row);
+            return lines;
+        }
+    }
+}
---
+++ Day1Challange2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    internal class SolutionDay1Challenge2
+    {
+        public static void Main(string[] args)
+        {
+            long solution = 0;
+            Console.WriteLine();
+            // Utils.ReadFile();
+            solution = Solution(Utils.ReadFile());
+            Console.WriteLine($"Solution to day 1 Challange 2: {solution}");
+        }
+
+        public static long Solution(List<int> numbers)
+        {
+            long result = 0;
+            bool breakfor = false;
+            foreach (int number in numbers)
+            {
+                int searchingNumber = 2020 - number;
+                foreach (int innernumber in numbers)
+                {
+                    if (numbers.Contains(searchingNumber - innernumber))
+                    {
+                        // Console.WriteLine($"{number} -- {innernumber} -- {searchingNumber-innernumber}");
+                        result = number * innernumber * (searchingNumber - innernumber);
+                        breakfor = true;
+                        break;
+                    }
+                }
+                if (breakfor)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    static class Utils
+    {
+        public static List<int> ReadFile()
+        {
+            List<int> lines =
+                new List<int>(
+                    File.ReadLines(@"C:\Przemek\Net and C#\AdventOfCode\Day1Challange2\input.txt").Count());
+            Console.WriteLine();
+            foreach (string line in File.ReadLines(@"C:\Przemek\Net and C#\AdventOfCode\Day1Challange2\input.txt"))
+            {
+                // Console.WriteLine(line);
+                lines.Add(int.Parse(line));
+            }
+            return lines;
+        }
+    }
+}
+
